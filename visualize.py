6c303ad1880e7c569bfe6731f2e52d85a9909dcb
sbite0138 <24842740+sbite0138@users.noreply.github.com>
2024-02-10 23:01:15 +0900

    update

---
+++ visualize.py
@@ -0,0 +1,47 @@
+from matplotlib import pyplot as plt
+import csv
+
+with open('result.csv', 'r') as file:
+    reader = csv.reader(file)
+    csv_data = list(reader)
+
+best_buy_price = {}
+best_sell_price = {}
+best_profit = {}
+for d in csv_data[1:]:
+    print(d)
+    id, buy_price, sell_price, profit = d
+    id = int(id)
+    buy_price = int(buy_price)
+    sell_price = int(sell_price)
+    profit = float(profit)
+    if id not in best_profit:
+        best_profit[id] = profit
+        best_buy_price[id] = buy_price
+        best_sell_price[id] = sell_price
+    elif profit > best_profit[id]:
+        best_profit[id] = profit
+        best_buy_price[id] = buy_price
+        best_sell_price[id] = sell_price
+
+for id in best_profit:
+    print(id, best_buy_price[id], best_sell_price[id], best_profit[id])
+
+# data = {}
+# for d in csv_data[1:]:
+#     print(d)
+#     id, buy_price, sell_price, profit = d
+#     id = int(id)
+#     buy_price = int(buy_price)
+#     sell_price = int(sell_price)
+#     profit = float(profit)
+#     if id not in data:
+#         data[id] = [[0 for _ in range(20)] for __ in range(20)]
+#     data[id][buy_price//5][sell_price//5-20] = profit
+
+# # put the data into single plot
+
+# for id in data:
+#     plt.imshow(data[id], cmap='hot', interpolation='nearest')
+#     plt.show()
+#     break
